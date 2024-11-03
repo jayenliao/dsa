98f700e98864a2f4449e5a9882db998c321ccf27
jayenliao <jay.liao@fazzfinancial.com>
2024-11-03 08:17:14 +0800

    feat: implement mergesort

--- sorting.py
+++ sorting.py
@@ -15,3 +15,33 @@ def insertion_sort(A:list):
             if A[j-1] <= A[j]: # If the previous element is not larger than the current element,
                 break          # meaning A[j] is loacted correctly, thus we break the nested loop.
             A[j], A[j-1] = A[j], A[j-1] # Otherwise, swap two elements.
+
+
+def merge_sort(A:list):
+    aux = [None] * len(A) # Set up an array with the same size as A
+
+    def rsort(low, high):
+        if high <= low: return # Basic condition
+        mid = (low + high) // 2 # Recursion: sort the left and right sub-arrays and merge them.
+        rsort(low, mid)
+        rsort(mid+1, high)
+        merge(low, mid, high)
+
+    def merge(low, mid, high):
+        aux[low:(high+1)] = A[low:(high+1)] # Duplicate sorted sub-array to aux.
+        left, right = low, mid+1 # Set up beginning indices for two sub-arrays.
+        for i in range(low, high+1):
+            if left > mid: # If the left sub-array has been empty, get a value from the right one.
+                A[i] = aux[right]
+                right += 1
+            elif right > high: # If the right sub-array has been empty, get a value from the left one.
+                A[i] = aux[left]
+                left += 1
+            elif aux[right] < aux[left]: # If the right value is smaller than the left one, get the right one.
+                A[i] = aux[right]
+                right += 1
+            else:                        # If the left value is smaller than the left one, get the left one.
+                A[i] = aux[left]
+                left += 1
+
+    rsort(0, len(A)-1)
